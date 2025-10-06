09294550cdb689bca588d92dd4b25b73cff5c006
Otávio Biagioni <otaviobiagioni31@gmail.com>
2025-10-06 18:01:57 -0300

    fix: recalculate score center in main loop

--- kobra.py
+++ kobra.py
@@ -216,9 +216,6 @@ def draw_grid():
             rect = pygame.Rect(x, y, GRID_SIZE, GRID_SIZE)
             pygame.draw.rect(arena, GRID_COLOR, rect, 1)
 
-score = BIG_FONT.render("1", True, MESSAGE_COLOR)
-score_rect = score.get_rect(center=(WIDTH/2, HEIGHT/20+HEIGHT/30))
-
 draw_grid()
 
 snake = Snake()    # The snake
@@ -280,6 +277,7 @@ while True:
 
     # Show score (snake length = head + tail)
     score = BIG_FONT.render(f"{len(snake.tail)}", True, SCORE_COLOR)
+    score_rect = score.get_rect(center=(WIDTH/2, HEIGHT/20+HEIGHT/30))
     arena.blit(score, score_rect)
 
     # If the head pass over an apple, lengthen the snake and drop another apple
