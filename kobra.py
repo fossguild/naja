5d6c7e933e9d2ae40ab882bd1cebcdfc4dc42fc8
clr <clrcera05@gmail.com>
2025-10-06 20:07:31 -0300

    format after fixing conflicts

--- kobra.py
+++ kobra.py
@@ -247,16 +247,25 @@ while True:
 
         # Key pressed
         if event.type == pygame.KEYDOWN:
-            if event.key in (pygame.K_DOWN, pygame.K_s):    # Down arrow (or S):  move down
+            if event.key in (
+                pygame.K_DOWN,
+                pygame.K_s,
+            ):  # Down arrow (or S):  move down
                 snake.ymov = 1
                 snake.xmov = 0
-            elif event.key in (pygame.K_UP, pygame.K_w):    # Up arrow (or W):    move up
+            elif event.key in (pygame.K_UP, pygame.K_w):  # Up arrow (or W):    move up
                 snake.ymov = -1
                 snake.xmov = 0
-            elif event.key in (pygame.K_RIGHT, pygame.K_d): # Right arrow (or D): move right
+            elif event.key in (
+                pygame.K_RIGHT,
+                pygame.K_d,
+            ):  # Right arrow (or D): move right
                 snake.ymov = 0
                 snake.xmov = 1
-            elif event.key in (pygame.K_LEFT, pygame.K_a):  # Left arrow (or A):  move left
+            elif event.key in (
+                pygame.K_LEFT,
+                pygame.K_a,
+            ):  # Left arrow (or A):  move left
                 snake.ymov = 0
                 snake.xmov = -1
             elif event.key == pygame.K_q:  # Q         : quit game
