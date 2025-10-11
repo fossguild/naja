3bffa3a0d67bd8a01369b2afc03327995eca2f21
Allan Silva <allanvss15@gmail.com>
2025-10-11 16:42:36 -0300

    code : add opacity to score display

--- kobra.py
+++ kobra.py
@@ -625,6 +625,7 @@ while True:
 
     # Show score (snake length = head + tail)
     score = BIG_FONT.render(f"{len(snake.tail)}", True, SCORE_COLOR)
+    score.set_alpha(75)  # opacity
     score_rect = score.get_rect(center=(WIDTH / 2, HEIGHT / 12))
     arena.blit(score, score_rect)
 
