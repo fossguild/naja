d6e62aa376180d8c9c9004a6462a3ed3da6ed81d
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2025-10-12 20:53:05 -0300

    doc: update inline documentation to English
    Improved code documentation by updating comments and docstrings
    to English for better consistency and international collaboration.

--- constants.py
+++ constants.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python3
-#
-#   Copyright (c) 2023, Monaco F. J. <monaco@usp.br>
-#
-#   This file is part of KobraPy.
-#
-#   KobraPy is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 """
 This file is for defining constants for the kobra.py game.
 This file is only for objects/constants whose values are known
