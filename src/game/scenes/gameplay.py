0714af64983dab27d29c5a9126d9ebdd9552697a
João Pedro Castelli <jotapcastelli@gmail.com>
2025-11-25 19:56:19 -0300

    fix little-bug of wrong import (#410)

--- src/game/scenes/gameplay.py
+++ src/game/scenes/gameplay.py
@@ -47,7 +47,7 @@ from ecs.systems.obstacle_generation import ObstacleGenerationSystem
 from ecs.systems.settings_apply import SettingsApplySystem
 from game.scenes.game_modes import get_resolved_game_mode
 from game.game_modes_registry import CLASSIC_MODE_NAME
-from src.game.settings import GameSettings
+from game.settings import GameSettings
 
 
 class GameplayScene(BaseScene):
