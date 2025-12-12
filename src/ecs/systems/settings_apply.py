0ca7a039e07a2d16e8231f99c1337048fbf21800
gusvidigal <gvschulgin@gmail.com>
2025-12-12 17:03:37 -0300

    ENHANCED: remove manual checks on minimum size

--- src/ecs/systems/settings_apply.py
+++ src/ecs/systems/settings_apply.py
@@ -155,8 +155,6 @@ class SettingsApplySystem(BaseSystem):
             world: ECS world instance
             desired_cells: Desired number of cells per side
         """
-        # ensure minimum size
-        desired_cells = max(5, int(desired_cells))
 
         # For autoplay mode, enforce even grid size (maze algorithm requires it)
         from game.game_modes_registry import AUTOPLAY_MODE_NAME
